5cffaba2381fa9bd78a626ecb1b55f289f10d178
AmeliVatore <70744454+AmeliVatore@users.noreply.github.com>
2021-05-30 02:13:10 +0300

    Add files via upload

---
+++ Pr_11/Pr_11/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace Pr_11
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.openButton = new System.Windows.Forms.Button();
+            this.magicButton = new System.Windows.Forms.Button();
+            this.saveButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(13, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(781, 402);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            // 
+            // openButton
+            // 
+            this.openButton.Location = new System.Drawing.Point(13, 421);
+            this.openButton.Name = "openButton";
+            this.openButton.Size = new System.Drawing.Size(75, 23);
+            this.openButton.TabIndex = 1;
+            this.openButton.Text = "Открыть";
+            this.openButton.UseVisualStyleBackColor = true;
+            this.openButton.Click += new System.EventHandler(this.openButton_Click);
+            // 
+            // magicButton
+            // 
+            this.magicButton.Location = new System.Drawing.Point(365, 421);
+            this.magicButton.Name = "magicButton";
+            this.magicButton.Size = new System.Drawing.Size(75, 23);
+            this.magicButton.TabIndex = 2;
+            this.magicButton.Text = "Магия";
+            this.magicButton.UseVisualStyleBackColor = true;
+            this.magicButton.Click += new System.EventHandler(this.magicButton_Click);
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(719, 421);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(75, 23);
+            this.saveButton.TabIndex = 3;
+            this.saveButton.Text = "Сохранить";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.magicButton);
+            this.Controls.Add(this.openButton);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Второй графический редактор";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button openButton;
+        private System.Windows.Forms.Button magicButton;
+        private System.Windows.Forms.Button saveButton;
+    }
+}
+
---
+++ Pr_11/Pr_11/Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pr_11
+{
+    public partial class Form1 : Form
+    {
+        private Point previousPoint, point;
+        private Bitmap bmp;
+        private Pen blackPen = new Pen(Color.Black, 4);
+        private Graphics g;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Image files (*.BMP, *.JPG, *.GIF, *.TIF, *.PNG, *.ICO, *.EMF, *.WMF)|*.bmp;*.jpg;*.gif; *.tif; *.png; *.ico; *.emf; *.wmf";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                Image image = Image.FromFile(ofd.FileName);
+
+                int width = image.Width;
+                int height = image.Height;
+                pictureBox1.Width = width;
+                pictureBox1.Height = height;
+
+                bmp = new Bitmap(image, width, height);
+                pictureBox1.Image = bmp;
+
+               g = Graphics.FromImage(pictureBox1.Image);
+            }
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image != null)
+            {
+                SaveFileDialog sfd = new SaveFileDialog();
+                sfd.Title = "Сохранить как...";
+                sfd.OverwritePrompt = true;     // показывать ли "Перезаписать файл" если пользователь указывает имя файла, который уже существует
+                sfd.CheckPathExists = true;     // отображает ли диалоговое окно предупреждение, если пользователь указывает путь, который не существует
+                sfd.Filter = "Bitmap File (*.bmp)|*.bmp|" + "Gif File(*.gif)|*.gif|" + "JPEG File(*.jpg)|*.jpg|" + "TIF File(*.tif)|*.tif|" + "PNG File(*.png)|*.png|" + "Image Files(*.*)|*.*";    // фильтр форматов файлов
+                sfd.ShowHelp = true;            // отображается ли кнопка Справка в диалоговом окне
+                if (sfd.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+                        pictureBox1.Image.Save(sfd.FileName);       // сохраняем изображение
+                    }
+                    catch
+                    {
+                        MessageBox.Show("Невозможно сохранить изображение", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+
+        }
+
+        private void magicButton_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image != null)
+            {
+                int x2 = bmp.Width / 2;
+                int x1 = x2 / 2;
+                int x3 = x1 + x2;
+                g.DrawLine(blackPen, x1, 0, x1, bmp.Height);
+                g.DrawLine(blackPen, x2, 0, x2, bmp.Height);
+                g.DrawLine(blackPen, x3, 0, x3, bmp.Height);
+
+                for (int j = 0; j < bmp.Height; j++)
+                    for (int i = 0; i < x1; i++)
+                    {
+                        UInt32 pixel = (UInt32)(bmp.GetPixel(i, j).ToArgb());
+                        float R = (float)((pixel & 0x00FF0000) >> 16);  // Красный
+                        float G = (float)((pixel & 0x0000FF00) >> 8);   // Зелёный
+                        float B = (float)((pixel & 0x000000FF));        // Голубой
+                        G = B = 0;  // (G + B) / 2.0f;
+                        UInt32 newPixel = 0xFF000000 | ((UInt32)R << 16) | ((UInt32)G << 8 | ((UInt32)B));
+                        bmp.SetPixel(i, j, Color.FromArgb((int)newPixel));
+                    }
+
+                for (int j = 0; j < bmp.Height; j++)
+                    for (int i = x1; i < x2; i++)
+                    {
+                        UInt32 pixel = (UInt32)(bmp.GetPixel(i, j).ToArgb());
+                        float R = (float)((pixel & 0x00FF0000) >> 16);  // Красный
+                        float G = (float)((pixel & 0x0000FF00) >> 8);   // Зелёный
+                        float B = (float)((pixel & 0x000000FF));        // Голубой
+                        R = B = 0;  // (R + B) / 2.0f;
+                        UInt32 newPixel = 0xFF000000 | ((UInt32)R << 16) | ((UInt32)G << 8 | ((UInt32)B));
+                        bmp.SetPixel(i, j, Color.FromArgb((int)newPixel));
+                    }
+
+                for (int j = 0; j < bmp.Height; j++)
+                    for (int i = x2; i < x3; i++)
+                    {
+                        UInt32 pixel = (UInt32)(bmp.GetPixel(i, j).ToArgb());
+                        float R = (float)((pixel & 0x00FF0000) >> 16);  // Красный
+                        float G = (float)((pixel & 0x0000FF00) >> 8);   // Зелёный
+                        float B = (float)((pixel & 0x000000FF));        // Голубой
+                        R = G = 0;  // (R + G) / 2.0f;
+                        UInt32 newPixel = 0xFF000000 | ((UInt32)R << 16) | ((UInt32)G << 8 | ((UInt32)B));
+                        bmp.SetPixel(i, j, Color.FromArgb((int)newPixel));
+                    }
+
+                for (int j = 0; j < bmp.Height; j++)
+                    for (int i = x3; i < bmp.Width; i++)
+                    {
+                        UInt32 pixel = (UInt32)(bmp.GetPixel(i, j).ToArgb());
+                        float R = (float)((pixel & 0x00FF0000) >> 16);
+                        float G = (float)((pixel & 0x0000FF00) >> 8);
+                        float B = (float)((pixel & 0x000000FF));                        
+                        R = G = B = (R + G + B) / 3.0f;
+                        UInt32 newPixel = 0XFF000000 | ((UInt32)R << 16) | ((UInt32)G << 8) | ((UInt32)B);
+                        bmp.SetPixel(i, j, Color.FromArgb((int)newPixel));
+                    }
+                pictureBox1.Image = bmp;
+            }
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            previousPoint.X = e.X;
+            previousPoint.Y = e.Y;
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                point.X = e.X;
+                point.Y = e.Y;
+
+                g.DrawLine(blackPen, previousPoint, point);
+
+                previousPoint.X = point.X;
+                previousPoint.Y = point.Y;
+                pictureBox1.Invalidate();
+            }
+        }
+    }
+}
